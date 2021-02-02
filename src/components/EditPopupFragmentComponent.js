a4ed693fd8ff98dce862721643bf0468c4d8b6bb
Noel Carlos Hernandez Perez <u0193667@lacaixa.es>
2021-02-02 18:27:02 +0100

    update

--- src/components/EditPopupFragmentComponent.js
+++ src/components/EditPopupFragmentComponent.js
@@ -14,6 +14,10 @@ class EditPopupFragmentComponent extends Component {
     if (this.props.onRef) this.props.onRef(this);
   }
 
+  componentDidUpdate() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+
   onLoad = async(id) => {
     if (this.props.onLoad !== undefined) {
       try {
