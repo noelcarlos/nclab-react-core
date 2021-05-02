bf0fdc8240bfc385998d21923420d691666848e8
Noel Carlos <noe.carlos@gmail.com>
2021-05-02 11:45:33 +0200

    update

--- src/components/AfeFormMessage.js
+++ src/components/AfeFormMessage.js
@@ -9,8 +9,15 @@ export class AfeFormMessage extends Component {
 
       return (<Fragment>
         {error && <div className="alert alert-danger" data-testid="formMessageAlert" role="formMessageAlert">{error}</div>}
-        {(loadState === LOADSTATE.LOADING) && <div className="alert alert-primary" data-testid="formMessageLoading" role="formMessageLoading">Loading...</div>}
-        {(submitState === SUBMITSTATE.SUBMITTING) && <div className="alert alert-primary" data-testid="formMessageSubmiting" role="formMessageSubmiting">Submitting...</div>}
+        {(loadState === LOADSTATE.LOADING) && 
+          <div className="alert alert-primary" data-testid="formMessageLoading" role="formMessageLoading">
+             <span class="spinner-border spinner-border-sm mr-2"></span>
+             Loading...
+          </div>}
+        {(submitState === SUBMITSTATE.SUBMITTING) && <div className="alert alert-primary" data-testid="formMessageSubmiting" role="formMessageSubmiting">
+          <span class="spinner-border spinner-border-sm mr-2"></span>
+          Submitting...
+        </div>}
       </Fragment>);
     }
   
