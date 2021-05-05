725f467a825a85a96099bff72c4b2d61ed14af67
Noel Carlos <noe.carlos@gmail.com>
2021-05-05 10:04:55 +0200

    fix className in AfeFormMessage

--- src/components/AfeFormMessage.js
+++ src/components/AfeFormMessage.js
@@ -11,11 +11,11 @@ export class AfeFormMessage extends Component {
         {error && <div className="alert alert-danger" data-testid="formMessageAlert" role="formMessageAlert">{error}</div>}
         {(loadState === LOADSTATE.LOADING) && 
           <div className="alert alert-primary" data-testid="formMessageLoading" role="formMessageLoading">
-             <span class="spinner-border spinner-border-sm mr-2"></span>
+             <span className="spinner-border spinner-border-sm mr-2"></span>
              Loading...
           </div>}
         {(submitState === SUBMITSTATE.SUBMITTING) && <div className="alert alert-primary" data-testid="formMessageSubmiting" role="formMessageSubmiting">
-          <span class="spinner-border spinner-border-sm mr-2"></span>
+          <span className="spinner-border spinner-border-sm mr-2"></span>
           Submitting...
         </div>}
       </Fragment>);
