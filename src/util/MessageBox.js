1b8f536967ca21b8d6daf17a9b8ab56b1fbb386d
Noel Carlos <noe.carlos@gmail.com>
2021-09-01 00:44:03 +0200

    update messsages

--- src/util/MessageBox.js
+++ src/util/MessageBox.js
@@ -2,110 +2,63 @@ import Swal from 'sweetalert2';
 
 export default class MessageBox {
 
-    static showDelete = (onAccept,onCancel) => {
-        Swal.fire({
+    static showDelete = async (onAccept,onCancel) => {
+        const result = await Swal.fire({
             title: '¿Desea archivar este registro?',
             text: 'El registro podrá ser recuperado posteriormente!',
             //type: 'warning',
             animation: false,
             showCancelButton: true,
-            confirmButtonText: 'Si, archivalo!',
+            confirmButtonText: 'Si',
             cancelButtonText: 'No',
             confirmButtonColor: '#2bc550',
             cancelButtonColor: '#6c757d'
-          }).then((result) => {
-            if (result.value) {
-              if (onAccept) { 
-                const promiseAccept = new Promise(async (resolve, reject) => {
-                    await onAccept();
-                    resolve();
-                });
-                
-                Promise.resolve(promiseAccept).then(function(onAcceptFunction) {
-                    //onAcceptFunction();
-                    Swal.fire({
-                        title: 'Archivado!',
-                        text: 'El registro ha sido archivado.',
-                        animation: false,
-                        //type: 'success',
-                        confirmButtonColor: '#2bc550',
-                        cancelButtonColor: '#6c757d'
-                    });
-                })
-              }
-              
-            // For more information about handling dismissals please visit
-            // https://sweetalert2.github.io/#handling-dismissals
-            } else if (result.dismiss === Swal.DismissReason.cancel) {
-                if (onCancel) {
-                    Promise.resolve(onCancel).then(function(value) {
-                        Swal.fire({
-                            //type: 'error',
-                            title: 'Cancelado',
-                            animation: false,
-                            text: 'El registro se ha conservado',
-                            confirmButtonColor: '#2bc550',
-                            cancelButtonColor: '#6c757d'
-                        });
-                    });
-                }
-            }
-          });
+        });
+          
+        if (result.value) {
+            await onAccept();
+
+            await Swal.fire({
+                title: 'Archivado!',
+                text: 'El registro ha sido archivado.',
+                animation: false,
+                //type: 'success',
+                confirmButtonColor: '#2bc550',
+                cancelButtonColor: '#6c757d'
+            });
+        }
+        
     }
 
-    static showRecover = (onAccept,onCancel) => {
-        Swal.fire({
+    static showRecover = async (onAccept) => {
+        const result = await Swal.fire({
             title: '¿Desea recuperar este registro?',
             text: 'El registro será activado definitivamente!',
             //type: 'warning',
             animation: false,
             showCancelButton: true,
-            confirmButtonText: 'Si, archivalo!',
+            confirmButtonText: 'Si',
             cancelButtonText: 'No',
             confirmButtonColor: '#2bc550',
             cancelButtonColor: '#6c757d'
-          }).then((result) => {
-            if (result.value) {
-              if (onAccept) { 
-                const promiseAccept = new Promise(async (resolve, reject) => {
-                    await onAccept();
-                    resolve();
-                });
+        });
+          
+        if (result.value) {
+            await onAccept();
 
-                Promise.resolve(promiseAccept).then(function(onAcceptFunction) {
-                    //onAcceptFunction();
-                    Swal.fire({
-                        title: 'Recuperado!',
-                        text: 'El registro ha sido recuperado.',
-                        animation: false,
-                        //type: 'success',
-                        confirmButtonColor: '#2bc550',
-                        cancelButtonColor: '#6c757d'
-                    });
-                })
-              }
-              
-            // For more information about handling dismissals please visit
-            // https://sweetalert2.github.io/#handling-dismissals
-            } else if (result.dismiss === Swal.DismissReason.cancel) {
-                if (onCancel) {
-                    Promise.resolve(onCancel).then(function(value) {
-                        Swal.fire({
-                            //type: 'error',
-                            title: 'Cancelado',
-                            animation: false,
-                            text: 'El registro se mantiene archivado',
-                            confirmButtonColor: '#2bc550',
-                            cancelButtonColor: '#6c757d'
-                        });
-                    });
-                }
-            }
-          });
+            await Swal.fire({
+                title: 'Recuperado!',
+                text: 'El registro ha sido recuperado.',
+                animation: false,
+                //type: 'success',
+                confirmButtonColor: '#2bc550',
+                cancelButtonColor: '#6c757d'
+            });
+        }
     }
 
-    static showConfirmation = (title, message) => {
-        Swal.fire({
+    static showConfirmation = async (title, message) => {
+        await Swal.fire({
             title: title,
             text: message,
             animation: false,
